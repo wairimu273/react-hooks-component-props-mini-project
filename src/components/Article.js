3361dec51815e4a0996231bdb4b910174b2944b5
Sarah <sarah.wairimu@student.moringaschool.com>
2025-04-26 09:40:55 +0300

    completes lab

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const getMinutesEmoji = () => {
+    if (minutes < 30) {
+      const cups = Math.ceil(minutes / 5);
+      return `${"☕️".repeat(cups)} ${minutes} min read`;
+    } else {
+      const boxes = Math.ceil(minutes / 10);
+      return `${"🍱".repeat(boxes)} ${minutes} min read`;
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {minutes && getMinutesEmoji()}
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
